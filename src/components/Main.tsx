5f1975323e3fafac7bc18c1d30c899ba2fa3be7a
miyatsuki <miyatsuki.shiku@gmail.com>
2023-02-20 00:42:35 +0900

    mod: ヘッダーをアプリ名に変更

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,3 @@
-import MenuIcon from '@mui/icons-material/Menu';
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 import { CardMedia } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
@@ -24,17 +23,8 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar component="nav">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Vsinger Xross Playlist
           </Typography>
           {client !== null ? <IconButton
             size="large"
